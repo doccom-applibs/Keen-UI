3979b48de8473eb28eadce9e8f5e22c0f3b168b8
doccom-applibs <liam.mcsheffrey@careflowconnect.com>
2018-05-17 14:56:02 +0100

    Added setTimeout on init of popver to fix scroll issues in side panels

--- src/UiPopover.vue
+++ src/UiPopover.vue
@@ -1,56 +1,37 @@
 <template>
-    <ui-focus-container
+    <div
         class="ui-popover"
-        ref="focusContainer"
         role="dialog"
+        tabindex="-1"
 
         :class="{ 'is-raised': raised }"
-        :contain-focus="containFocus"
-        :focus-redirector="focusRedirector"
 
-        @focus-overflow="close"
-        @keydown.native.esc="close"
+        @keydown.esc="closeDropdown"
     >
         <slot></slot>
-    </ui-focus-container>
+        <div class="ui-popover__focus-redirector" tabindex="0" @focus="restrictFocus"></div>
+    </div>
 </template>
 
 <script>
 import classlist from './helpers/classlist';
 import Drop from 'tether-drop';
 
-import UiFocusContainer from './UiFocusContainer.vue';
-
 export default {
     name: 'ui-popover',
 
     props: {
         trigger: {
-            validator(value) {
-                const isValid = (value instanceof Element) || (value && value._isVue) || (typeof value === 'string');
-
-                if (!isValid) {
-                    console.warn('[UiPopover]: Invalid prop: "trigger". Expected Element, VueComponent or CSS selector string which matches an existing element.');
-                }
-
-                return isValid;
-            }
+            type: String,
+            required: true
         },
-        position: {
+        dropdownPosition: {
             type: String,
             default: 'bottom left'
-        },
-        constrainToScrollParent: {
-            type: Boolean,
-            default: true
         },
         openOn: {
             type: String,
             default: 'click' // 'click', 'hover', 'focus', or 'always'
-        },
-        removeOnClose: {
-            type: Boolean,
-            default: false
         },
         containFocus: {
             type: Boolean,
@@ -63,65 +44,47 @@ export default {
         }
     },
 
-    watch: {
-        trigger() {
-            this.destroyDrop();
-            this.setupDrop();
+    data() {
+        return {
+            dropInstance: null,
+            lastfocusedElement: null
+        };
+    },
+
+    computed: {
+        triggerEl() {
+            return this.$parent.$refs[this.trigger];
         }
     },
 
-    created() {
-        // Instance data, not declared in data() as we don't want reactivity.
-        this.triggerEl = null;
-        this.dropInstance = null;
-        this.lastFocusedElement = null;
-    },
-
     mounted() {
-        this.setupDrop();
+        if (this.triggerEl) {
+            setTimeout(() => {//Fixes issue where dropdown does not scroll in certain situations                              
+                this.initializeDropdown();
+            },0)
+           
+        }
     },
 
     beforeDestroy() {
-        this.destroyDrop();
+        if (this.dropInstance) {
+            this.dropInstance.destroy();
+        }
     },
 
     methods: {
-        setTrigger() {
-            if (this.trigger instanceof Element) {
-                this.triggerEl = this.trigger;
-            } else if (this.trigger && this.trigger._isVue) {
-                this.triggerEl = this.trigger.$el;
-            } else if (typeof this.trigger === 'string') {
-                this.triggerEl = document.querySelector(this.trigger);
-            }
-
-            // Fallback to using the parent (DOM parent, not Vue component parent)
-            // if triggerEl is invalid
-            if (!(this.triggerEl instanceof Element)) {
-                this.triggerEl = this.$el.parentElement;
-            }
-        },
-
-        setupDrop() {
-            this.setTrigger();
-
-            if (!this.triggerEl) {
-                return;
-            }
-
+        initializeDropdown() {
             this.dropInstance = new Drop({
                 target: this.triggerEl,
                 content: this.$el,
-                position: this.position,
+                position: this.dropdownPosition,
                 constrainToWindow: true,
-                openOn: this.openOn,
-                remove: this.removeOnClose,
-                constrainToScrollParent: this.constrainToScrollParent
+                openOn: this.openOn
             });
 
             // TO FIX: Workaround for Tether not positioning
             // correctly for positions other than 'bottom left'
-            if (!this.removeOnClose && this.dropdownPosition !== 'bottom left') {
+            if (this.dropdownPosition !== 'bottom left') {
                 this.dropInstance.open();
                 this.dropInstance.close();
                 this.dropInstance.open();
@@ -132,33 +95,22 @@ export default {
             this.dropInstance.on('close', this.onClose);
         },
 
-        destroyDrop() {
-            if (this.dropInstance) {
-                this.dropInstance.destroy();
-                this.dropInstance = null;
-            }
-        },
-
-        open() {
+        openDropdown() {
             if (this.dropInstance) {
                 this.dropInstance.open();
             }
         },
 
-        close() {
+        closeDropdown() {
             if (this.dropInstance) {
                 this.dropInstance.close();
             }
         },
 
-        toggle() {
+        toggleDropdown(e) {
             if (this.dropInstance) {
-                this.dropInstance.toggle();
+                this.dropInstance.toggle(e);
             }
-        },
-
-        isOpen() {
-            return Boolean(this.dropInstance) && this.dropInstance.isOpened();
         },
 
         /**
@@ -188,8 +140,8 @@ export default {
             this.positionDrop();
             classlist.add(this.triggerEl, 'has-dropdown-open');
 
-            this.lastFocusedElement = document.activeElement;
-            this.$refs.focusContainer.focus();
+            this.lastfocusedElement = document.activeElement;
+            this.$el.focus();
 
             this.$emit('open');
         },
@@ -197,16 +149,39 @@ export default {
         onClose() {
             classlist.remove(this.triggerEl, 'has-dropdown-open');
 
-            if (this.lastFocusedElement) {
-                this.lastFocusedElement.focus();
+            if (this.lastfocusedElement) {
+                this.lastfocusedElement.focus();
             }
 
             this.$emit('close');
-        }
-    },
+        },
 
-    components: {
-        UiFocusContainer
+        restrictFocus(e) {
+            if (!this.containFocus) {
+                this.closeDropdown();
+                return;
+            }
+
+            e.stopPropagation();
+
+            if (this.focusRedirector) {
+                this.focusRedirector(e);
+            } else {
+                this.$el.focus();
+            }
+        },
+
+        open() {
+            this.openDropdown();
+        },
+
+        close() {
+            this.closeDropdown();
+        },
+
+        toggle() {
+            this.toggleDropdown();
+        }
     }
 };
 </script>
@@ -227,6 +202,11 @@ export default {
     .ui-menu {
         border: none;
     }
+}
+
+.ui-popover__focus-redirector {
+    opacity: 0;
+    position: absolute;
 }
 
 .drop-element {
